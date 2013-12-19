7f8851f6151df5a1cdd3a55410859c361adb38b3
Rahul Thakur <rahul.thakur@sourcebits.com>
2013-12-19 19:37:15 +0530

    del v
    s

--- views/admin/footer_trial_message.php
+++
@@ -1,6 +0,0 @@
-<?php if(isset($_SESSION['free_user']) && $_SESSION['free_user'] == 1) {?>
-		<div class="expiry-message">
-			<label class="msg">You have <?php echo $_SESSION['days_left']?> day(s) left. Please contact your company
-			Administrator to upgrade your plan.</label>
-		</div>
-<?php }?>
--- views/admin/temp_dropbox_view.php
+++
@@ -1,6 +0,0 @@
-
-<script type="text/javascript">
-//window.open ('<?php echo $url;?>', "Dropbox Authentication","location=1,status=1,scrollbars=1, width=853,height=630, left=" + (screen.width - 853)/2 + ",top=" + (screen.height - 630)/2);
-//window.location.href='<?php echo $url;?>';
-
-</script>
